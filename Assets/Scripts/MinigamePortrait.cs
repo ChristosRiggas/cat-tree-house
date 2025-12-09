b1ef56ee9faa53d2059e2bb9b4ba7cf04541a379
Christos Riggas <124679358+ChristosRiggas@users.noreply.github.com>
2025-12-09 17:05:51 +0100

    Timer is persistent across scenes for the portraits

--- Assets/Scripts/MinigamePortrait.cs
+++ Assets/Scripts/MinigamePortrait.cs
@@ -1,25 +1,154 @@
-using System.Collections;
-using Unity.VisualScripting;
+//using System.Collections;
+//using Unity.VisualScripting;
+//using UnityEngine;
+//using UnityEngine.EventSystems;
+//using UnityEngine.UI;
+
+//public class MinigamePortrait : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+//{
+//    [Header("Cooldown")]
+//    public int cooldownTime = 5;
+
+//    private bool available = false;
+//    private bool isOnCooldown = false;
+
+//    private Button button;
+
+//    public string sceneToLoad;
+//    public int requiredHeight = 10;
+
+//    [SerializeField] private GameObject descriptionPanel;
+
+//    void Awake()
+//    {
+//        button = GetComponentInChildren<Button>();
+//        button.interactable = false;
+//        button.onClick.AddListener(OnButtonPressed);
+//    }
+
+//    private void Start()
+//    {
+//        if (descriptionPanel != null)
+//            descriptionPanel.SetActive(false);
+
+//        StartCooldown();
+//    }
+
+
+//    public void OnPointerEnter(PointerEventData eventData)
+//    {
+//        if (available)
+//        {
+//            if (descriptionPanel != null)
+//                descriptionPanel.SetActive(true);
+//        }
+//    }
+
+//    public void OnPointerExit(PointerEventData eventData)
+//    {
+//        if (available)
+//        {
+//            if (descriptionPanel != null)
+//                descriptionPanel.SetActive(false);
+//        }
+//    }
+
+//    private void Update()
+//    {
+//        CheckIfTheMinigameIsAvailable();
+
+//        if (UpgradePartManager.Instance.upgradeMode || isOnCooldown)
+//        {
+//            descriptionPanel.SetActive(false);
+//        }
+//    }
+
+//    public void CheckIfTheMinigameIsAvailable()
+//    {
+//        if(UpgradePartManager.Instance != null && UpgradePartManager.Instance.upgradeMode)
+//        {
+//            available = false;
+//            UpdateButtonState();
+//            GetComponentInParent<Canvas>().sortingOrder = -9;
+//            return;
+//        }
+
+//        if (requiredHeight <= CurrencyManager.Instance.treeHeight)
+//        {
+//            available = true;
+//        }
+//        else
+//        {
+//            available = false;
+//        }
+
+//        Canvas canvas = GetComponentInParent<Canvas>();
+
+//        if (available && !UpgradePartManager.Instance.upgradeMode)
+//            canvas.sortingOrder = -9; // 100 before but looks weird
+//        else
+//            canvas.sortingOrder = -9;
+
+//        UpdateButtonState();
+//    }
+
+//    // ---------- BUTTON ----------
+
+//    private void OnButtonPressed()
+//    {
+//        if (!button.interactable) return;
+
+//        GameSceneManager.Instance.ChangeScene(sceneToLoad);
+//        StartCooldown();
+//    }
+
+//    // ---------- COOLDOWN ----------
+
+//    private void StartCooldown()
+//    {
+//        isOnCooldown = true;
+//        UpdateButtonState();
+//        StartCoroutine(CooldownTimer());
+//    }
+
+//    IEnumerator CooldownTimer()
+//    {
+//        yield return new WaitForSeconds(cooldownTime);
+
+//        isOnCooldown = false;
+//        UpdateButtonState();
+//    }
+
+//    // ---------- STATE ----------
+
+//    private void UpdateButtonState()
+//    {
+//        button.interactable = available && !isOnCooldown;
+//    }
+//}
+
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MinigamePortrait : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    [Header("Cooldown")]
-    public int cooldownTime = 5;
-
-    private bool available = false;
-    private bool isOnCooldown = false;
-
-    private Button button;
-
+    [Header("Minigame")]
     public string sceneToLoad;
     public int requiredHeight = 10;
 
-    [SerializeField] private GameObject descriptionPanel;
+    [Header("Cooldown")]
+    public int cooldownTime = 5;
 
-    void Awake()
+    [Header("UI")]
+    [SerializeField] private GameObject descriptionPanel;
+    [SerializeField] private TMP_Text cooldownText;
+
+    private Button button;
+    private bool available = false;
+
+    private void Awake()
     {
         button = GetComponentInChildren<Button>();
         button.interactable = false;
@@ -31,13 +160,37 @@ public class MinigamePortrait : MonoBehaviour, IPointerEnterHandler, IPointerExi
         if (descriptionPanel != null)
             descriptionPanel.SetActive(false);
 
-        StartCooldown();
+        UpdateButtonState();
     }
 
+    private void Update()
+    {
+        CheckAvailability();
+
+        // Hide description if not usable
+        if (!available || IsOnCooldown() || UpgradePartManager.Instance.upgradeMode)
+        {
+            if (descriptionPanel != null)
+                descriptionPanel.SetActive(false);
+        }
+
+        float remaining = MinigamePortraitsTimeManager.Instance
+                    .GetRemainingTime(sceneToLoad);
+
+        if (IsOnCooldown())
+        {
+            cooldownText.text = $"{remaining:F1}s";
+        }
+        else
+        {
+            cooldownText.text = "Ready";
+        }
+    }
+    // ---------- HOVER ----------
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (available)
+        if (available && !IsOnCooldown())
         {
             if (descriptionPanel != null)
                 descriptionPanel.SetActive(true);
@@ -46,49 +199,22 @@ public class MinigamePortrait : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        if (available)
-        {
-            if (descriptionPanel != null)
-                descriptionPanel.SetActive(false);
-        }
-    }
-
-    private void Update()
-    {
-        CheckIfTheMinigameIsAvailable();
-
-        if (UpgradePartManager.Instance.upgradeMode || isOnCooldown)
-        {
+        if (descriptionPanel != null)
             descriptionPanel.SetActive(false);
-        }
     }
 
-    public void CheckIfTheMinigameIsAvailable()
+    // ---------- AVAILABILITY ----------
+
+    private void CheckAvailability()
     {
-        if(UpgradePartManager.Instance != null && UpgradePartManager.Instance.upgradeMode)
+        if (UpgradePartManager.Instance != null && UpgradePartManager.Instance.upgradeMode)
         {
             available = false;
             UpdateButtonState();
-            GetComponentInParent<Canvas>().sortingOrder = -9;
             return;
         }
 
-        if (requiredHeight <= CurrencyManager.Instance.treeHeight)
-        {
-            available = true;
-        }
-        else
-        {
-            available = false;
-        }
-
-        Canvas canvas = GetComponentInParent<Canvas>();
-
-        if (available && !UpgradePartManager.Instance.upgradeMode)
-            canvas.sortingOrder = -9; // 100 before but looks weird
-        else
-            canvas.sortingOrder = -9;
-
+        available = CurrencyManager.Instance.treeHeight >= requiredHeight;
         UpdateButtonState();
     }
 
@@ -98,31 +224,25 @@ public class MinigamePortrait : MonoBehaviour, IPointerEnterHandler, IPointerExi
     {
         if (!button.interactable) return;
 
+        MinigamePortraitsTimeManager.Instance.StartCooldown(
+            sceneToLoad,
+            cooldownTime
+        );
+
         GameSceneManager.Instance.ChangeScene(sceneToLoad);
-        StartCooldown();
-    }
-
-    // ---------- COOLDOWN ----------
-
-    private void StartCooldown()
-    {
-        isOnCooldown = true;
-        UpdateButtonState();
-        StartCoroutine(CooldownTimer());
-    }
-
-    IEnumerator CooldownTimer()
-    {
-        yield return new WaitForSeconds(cooldownTime);
-
-        isOnCooldown = false;
-        UpdateButtonState();
     }
 
     // ---------- STATE ----------
 
+    private bool IsOnCooldown()
+    {
+        return MinigamePortraitsTimeManager.Instance
+            .IsOnCooldown(sceneToLoad);
+    }
+
     private void UpdateButtonState()
     {
-        button.interactable = available && !isOnCooldown;
+        button.interactable = available && !IsOnCooldown();
     }
 }
+
---
+++ Assets/Scripts/MinigamePortraitsTimeManager.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigamePortraitsTimeManager : MonoBehaviour
+{
+    public static MinigamePortraitsTimeManager Instance;
+
+    // Key = minigame ID (scene name)
+    // Value = time when cooldown ends
+    private Dictionary<string, float> cooldownEndTimes = new();
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+        foreach (var kvp in cooldownEndTimes)
+        {
+            float remaining = Mathf.Max(0, kvp.Value - Time.time);
+            Debug.Log($"Minigame: {kvp.Key} | Remaining cooldown: {remaining:F2}s");
+        }
+    }
+
+    // Start cooldown for a minigame
+    public void StartCooldown(string minigameID, float cooldownDuration)
+    {
+        cooldownEndTimes[minigameID] = Time.time + cooldownDuration;
+    }
+
+    // Check if a minigame is on cooldown
+    public bool IsOnCooldown(string minigameID)
+    {
+        if (!cooldownEndTimes.ContainsKey(minigameID))
+            return false;
+
+        return Time.time < cooldownEndTimes[minigameID];
+    }
+
+    // Optional: remaining cooldown time
+    public float GetRemainingTime(string minigameID)
+    {
+        if (!cooldownEndTimes.ContainsKey(minigameID))
+            return 0f;
+
+        return Mathf.Max(0f, cooldownEndTimes[minigameID] - Time.time);
+    }
+}
